415277c43724b1f1ce3ec7ef299b7372db86f76f
Dustin Morgan <dustin_morgan@cable.comcast.com>
2015-09-03 17:46:17 -0600

    wordpress x64

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -6,18 +6,18 @@ class wordpress::packages (
   if ($manage_php_repo == true and $manage_epel_repo == true) {
     yumrepo { "remi":
       descr    => "Remi's PHP 5.6 RPM repository for Enterprise Linux 6",
-      baseurl  => "http://rpms.remirepo.net/enterprise/6/php56/mirror",
+      baseurl  => "http://rpms.remirepo.net/enterprise/6/php56/x86_64/",
       gpgcheck => "0",
       enabled  => "1"
     } ->
     yumrepo { "remi-safe":
       descr    => "Safe Remi's RPM repository for Enterprise Linux 6 ",
-      baseurl  => "http://rpms.remirepo.net/enterprise/6/safe/mirror",
+      baseurl  => "http://rpms.remirepo.net/enterprise/6/safe/x86_64/",
       gpgcheck => "0",
       enabled  => "1"
     } -> yumrepo { "epel":
-      descr    => "Extra Packages for Enterprise Linux 6 - \$basearch",
-      baseurl  => "http://download.fedoraproject.org/pub/epel/6/\$basearch",
+      descr    => "Extra Packages for Enterprise Linux 6 ",
+      baseurl  => "http://download.fedoraproject.org/pub/epel/6/x86_64/",
       gpgcheck => "0",
       enabled  => "1"
     } ->
