e2d47db11df04da6b665a75a5cdc3a48676cb83b
Dustin Morgan <dustin_morgan@cable.comcast.com>
2015-09-03 17:40:04 -0600

    package params

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,8 +5,6 @@
 # Parameters:
 #
 class wordpress (
-  $manage_php_repo  = $::wordpress::params::manage_php_repo,
-  $manage_epel_repo = $::wordpress::params::manage_epel_repo,
   $site_name        = $::wordpress::params::site_name,
   $dir_wordpress    = $::wordpress::params::dir_wordpress,
   $url_source       = $::wordpress::params::url_source,
@@ -19,31 +17,6 @@ class wordpress (
   require wordpress::packages
   include ::apache
   include ::apache::php
-
-  if ($manage_php_repo) {
-    yumrepo { "remi":
-      descr    => "Remi's PHP 5.6 RPM repository for Enterprise Linux 6",
-      baseurl  => "http://rpms.remirepo.net/enterprise/6/php56/mirror",
-      gpgcheck => "0",
-      enabled  => "1"
-    }
-
-    yumrepo { "remi-safe":
-      descr    => "Safe Remi's RPM repository for Enterprise Linux 6 ",
-      baseurl  => "http://rpms.remirepo.net/enterprise/6/safe/mirror",
-      gpgcheck => "0",
-      enabled  => "1"
-    }
-  }
-
-  if ($manage_epel_repo) {
-    yumrepo { "epel":
-      descr    => "Extra Packages for Enterprise Linux 6 - \$basearch",
-      baseurl  => "http://download.fedoraproject.org/pub/epel/6/\$basearch",
-      gpgcheck => "0",
-      enabled  => "1"
-    }
-  }
 
   artifact { 'wordpress.tar.gz':
     source => $url_source,
