6f3c791653d828d8f6bd97ed46f0a335fda1069a
Dustin Morgan <dustin_morgan@cable.comcast.com>
2015-09-03 17:46:58 -0600

    repos

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -42,16 +42,16 @@ class wordpress::packages (
   } elsif ($manage_php_repo == true and $manage_epel_repo == false) {
     yumrepo { "remi":
       descr    => "Remi's PHP 5.6 RPM repository for Enterprise Linux 6",
-      baseurl  => "http://rpms.remirepo.net/enterprise/6/php56/mirror",
+      baseurl  => "http://rpms.remirepo.net/enterprise/6/php56/x86_64/",
       gpgcheck => "0",
       enabled  => "1"
     } ->
     yumrepo { "remi-safe":
       descr    => "Safe Remi's RPM repository for Enterprise Linux 6 ",
-      baseurl  => "http://rpms.remirepo.net/enterprise/6/safe/mirror",
+      baseurl  => "http://rpms.remirepo.net/enterprise/6/safe/x86_64/",
       gpgcheck => "0",
       enabled  => "1"
-    } ->
+    }  ->
     package { [
       'php',
       'php-pecl-apcu',
@@ -72,11 +72,11 @@ class wordpress::packages (
     }
   } elsif ($manage_epel_repo == true and $manage_php_repo == false) {
     yumrepo { "epel":
-      descr    => "Extra Packages for Enterprise Linux 6 - \$basearch",
-      baseurl  => "http://download.fedoraproject.org/pub/epel/6/\$basearch",
+      descr    => "Extra Packages for Enterprise Linux 6 ",
+      baseurl  => "http://download.fedoraproject.org/pub/epel/6/x86_64/",
       gpgcheck => "0",
       enabled  => "1"
-    } ->
+    }  ->
     package { [
       'php',
       'php-pecl-apcu',
