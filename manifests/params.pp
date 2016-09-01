07f367e24ca3c6514687b2bac942cb70869eaf6c
Dustin Morgan <morgan@aspendenver.org>
2016-09-01 15:24:28 -0600

    more better foo
    bar

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,15 +1,24 @@
 # class wordpress::params
 class wordpress::params {
-  $manage_php_repo = true
-  $manage_epel_repo = true
-  $site_name = 'www.wordpress.com'
+  $manage_php_repo = false
+  $manage_epel_repo = false
+  case $::fqdn {
+    /^foo.*/     : { 
+        $site_name = 'www.foo.com'
+        $mysql_pass = '9p48ryUJKUPUKFVf8o3pg4'
+    }
+    /^bar.*/     : { 
+        $site_name = 'www.bar.com'
+        $mysql_pass = '31e1dfaKJBPUHV00oijalv'
+    }
+  }
+            
   $dir_wordpress = '/opt/wordpress'
   $url_source = 'https://wordpress.org/latest.tar.gz'
   $mysql_db = 'wordpress'
   $mysql_host = 'localhost'
   $mysql_user = 'wordpress'
-  $mysql_pass = 'w0rdPR355p4ssw0rd4mysql'
-  $mysql_root = '6vrv7ftRTCFGVBHK4f4gfer5gqe'
+  
   $owner = 'wordpress'
   $group = 'wordpress'
 }
